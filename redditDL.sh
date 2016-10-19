30520a805970b1b6c162cd994ed8bbdc94a4216f
mark <mark@darwin>
2016-10-19 18:58:20 +0300

    intial upload

---
+++ redditDL.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+urlist=$(wget -q -O - "$1" | sed -e "s/</\\n/g" | grep "title may-blank" | cut -d '"' -f 4)
+
+usrname=$(echo $1 | cut -d "/" -f 5)
+mkdir -p $usrname
+cd $usrname
+mkdir -p images
+mkdir -p gifs
+
+for var in $urlist
+do
+  if [[ $var == *"i.imgu"* ]]; then
+   wget -nc -nv -P ./images/ $var
+  elif [[ $var == *"imgur.com/a/"* ]]; then
+    aID=$(echo $var | cut -d "/" -f 5)
+    wget -nc -nv -O ./images/$aID.zip "https://s.imgur.com/a/$aID/zip"
+  elif [[ $var == *"gfyca"* ]]; then
+    wget -nc -nv -P ./gifs/ $(wget -q -O - $var | sed -e "s/</\\n/g" | grep webmSource | cut -d '"' -f 4)
+  fi  
+done
+
+nextpg=$(wget -q -O - "$1" | sed -e "s/</\\n/g" | grep "nofollow next")
+
+if [ $? -eq 0 ]; then
+  cd ..
+  ./redditDL.sh $(echo "$nextpg" | cut -d '"' -f 2)
+else
+  cd images
+  for f in *.zip; do
+    mkdir $(echo $f | cut -d "." -f 1) && unzip -d $(echo $f | cut -d "." -f 1) $f && rm $f
+  done
+fi
